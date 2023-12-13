31c2a95489462acc1289eed11f858f22c6c7349b
BartoszNitschke <s25771@pjwstk.edu.pl>
2023-12-13 15:54:26 +0100

    1

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Profiles from "./components/Profiles";
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="">Siema</h1>
+      <Profiles></Profiles>
+    </div>
+  );
+}
+
+export default App;
